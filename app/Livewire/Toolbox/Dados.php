26fa46ea03e08687ddb048b9ee525268af69395d
lauranascimentoo <laurans1003@gmail.com>
2025-05-06 22:41:08 -0300

    first commit

---
+++ app/Livewire/Toolbox/Dados.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Toolbox;
+
+use Livewire\Component;
+
+class Dados extends Component
+{
+    public function render()
+    {
+        return view('livewire.toolbox.dados');
+    }
+}
---
+++ app/Livewire/Toolbox/Lista.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Toolbox;
+
+use Livewire\Component;
+
+class Lista extends Component
+{
+    public function render()
+    {
+        return view('livewire.toolbox.lista');
+    }
+}
