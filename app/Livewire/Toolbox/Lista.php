70dba2fd6640fcd989c7aeb6664e815e60e82e0b
lauranascimentoo <laurans1003@gmail.com>
2025-05-06 23:05:57 -0300

    versão incial base - tela e botões

--- app/Livewire/Toolbox/Lista.php
+++ app/Livewire/Toolbox/Lista.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace App\Livewire\Toolbox;
+namespace App\Http\Controllers\Livewire\Toolbox;
 
 use Livewire\Component;
+use App\Models\Software;
 
 class Lista extends Component
 {
+    public $softwares;
+
+    public function mount()
+    {
+        $this->softwares = Software::all();
+    }
+
     public function render()
     {
         return view('livewire.toolbox.lista');
--- resources/views/livewire/toolbox/lista.blade.php
+++ resources/views/livewire/toolbox/lista.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="container" style="padding: 20px;">
+    <h2 style="margin-bottom: 10px;">Lista de Softwares</h2>
+
+    <button style="margin-bottom: 10px; padding: 6px 12px; background-color: #007bff; color: white; border: none; border-radius: 4px;">
+        Novo
+    </button>
+
+    <table border="1" cellpadding="8" cellspacing="0" style="width: 100%; border-collapse: collapse;">
+        <thead>
+            <tr style="background-color: #f1f1f1;">
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Versão</th>
+                <th>Descrição</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($softwares as $software)
+                <tr>
+                    <td>{{ $software->id }}</td>
+                    <td>{{ $software->nome }}</td>
+                    <td>{{ $software->versao }}</td>
+                    <td>{{ $software->descricao }}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4">Nenhum software cadastrado.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </div>
